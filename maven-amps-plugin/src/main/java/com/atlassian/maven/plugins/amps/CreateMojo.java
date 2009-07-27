d3c9461f155651d9da8c3c3274645f6bbcc47966
dbrown <dbrown@atlassian.com>
2009-07-27 02:39:25 +0000

    Fixing create and idea mojos to not require project
    git-svn-id: https://studio.atlassian.com/svn/AMPS/trunk@40585 2c54a935-e501-0410-bc05-97a93f6bca70

--- maven-amps-plugin/src/main/java/com/atlassian/maven/plugins/amps/CreateMojo.java
+++ maven-amps-plugin/src/main/java/com/atlassian/maven/plugins/amps/CreateMojo.java
@@ -4,12 +4,13 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.jfrog.maven.annomojo.annotations.MojoGoal;
 import org.jfrog.maven.annomojo.annotations.MojoRequiresProject;
+import org.jfrog.maven.annomojo.annotations.MojoRequiresDirectInvocation;
 
 /**
  * Creates a new plugin
  */
 @MojoGoal("create")
-@MojoRequiresProject
+@MojoRequiresProject(false)
 public class CreateMojo extends AbstractProductAwareMojo
 {
     public void execute() throws MojoExecutionException, MojoFailureException
--- maven-amps-plugin/src/main/java/com/atlassian/maven/plugins/amps/cli/IdeaMojo.java
+++ maven-amps-plugin/src/main/java/com/atlassian/maven/plugins/amps/cli/IdeaMojo.java
@@ -4,8 +4,10 @@ import com.atlassian.maven.plugins.amps.AbstractAmpsMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.jfrog.maven.annomojo.annotations.MojoGoal;
+import org.jfrog.maven.annomojo.annotations.MojoRequiresProject;
 
 @MojoGoal("idea")
+@MojoRequiresProject(false)
 public class IdeaMojo extends AbstractAmpsMojo
 {
     public void execute() throws MojoExecutionException, MojoFailureException
