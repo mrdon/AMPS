d1e68c30804ce7e3aea8720f781ebbf144652b0d
dbrown <dbrown@atlassian.com>
2009-08-21 00:44:04 +0000

    Add detection of product id from which plugin is being used AMPS-56
    git-svn-id: https://studio.atlassian.com/svn/AMPS/trunk@42188 2c54a935-e501-0410-bc05-97a93f6bca70

--- maven-amps-plugin/src/main/java/com/atlassian/maven/plugins/amps/AbstractProductAwareMojo.java
+++ maven-amps-plugin/src/main/java/com/atlassian/maven/plugins/amps/AbstractProductAwareMojo.java
@@ -1,8 +1,15 @@
 package com.atlassian.maven.plugins.amps;
 
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.model.Plugin;
 import org.jfrog.maven.annomojo.annotations.MojoParameter;
 import com.atlassian.maven.plugins.amps.product.ProductHandlerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.atlassian.maven.plugins.amps.product.ProductHandlerFactory;
+import com.atlassian.maven.plugins.amps.product.RefappProductHandler;
 
 public abstract class AbstractProductAwareMojo extends AbstractAmpsMojo
 {
@@ -22,9 +29,27 @@ public abstract class AbstractProductAwareMojo extends AbstractAmpsMojo
         if (product == null)
         {
             product = getDefaultProductId();
+            if (product == null && project != null)
+            {
+                for (Plugin plugin : (List<Plugin>)project.getBuild().getPlugins())
+                {
+                    if ("com.atlassian.maven.plugins".equals(plugin.getGroupId()))
+                    {
+                        for (String productKey : ProductHandlerFactory.getIds())
+                        {
+                            if (("maven-"+productKey+"-plugin").equals(plugin.getArtifactId()))
+                            {
+                                product = productKey;
+                                break;
+                            }
+                        }
+
+                    }
+                }
+            }
             if (product == null)
             {
-                return ProductHandlerFactory.REFAPP;
+                product = ProductHandlerFactory.REFAPP;
             }
         }
         return product;
