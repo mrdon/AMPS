20c7648f0b64c54c4fca13c7458fa32cbb0b5d86
sleberrigaud <sleberrigaud@atlassian.com>
2009-08-24 23:36:06 +0000

    AMPS-36 testing copy-bundled-dependencies goal #resolve
    git-svn-id: https://studio.atlassian.com/svn/AMPS/trunk@42384 2c54a935-e501-0410-bc05-97a93f6bca70

---
+++ maven-amps-plugin/src/it/copyBundledDependencies/validate.groovy
@@ -0,0 +1,7 @@
+def metaInfLib = new File(basedir, 'target/classes/META-INF/lib')
+assert metaInfLib.exists(), "The $metaInfLib directory should exist"
+
+assert new File(metaInfLib, 'commons-io-1.4.jar').exists()
+assert new File(metaInfLib, 'commons-logging-1.1.1.jar').exists()
+assert !new File(metaInfLib, 'servlet-api-2.4.jar').exists()
+assert !new File(metaInfLib, 'junit-4.5.jar').exists()
