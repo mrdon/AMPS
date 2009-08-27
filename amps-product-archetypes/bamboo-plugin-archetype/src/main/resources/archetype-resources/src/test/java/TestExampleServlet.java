0603d3815f23e5c0f692286c8f8f5d0a306985c3
pkuo <pkuo@atlassian.com>
2009-08-27 06:32:08 +0000

    AMPS-84 bamboo archetype
    git-svn-id: https://studio.atlassian.com/svn/AMPS/trunk@42696 2c54a935-e501-0410-bc05-97a93f6bca70

---
+++ amps-product-archetypes/bamboo-plugin-archetype/src/main/resources/archetype-resources/src/test/java/TestExampleServlet.java
@@ -0,0 +1,14 @@
+package ${package};
+
+import junit.framework.TestCase;
+
+/**
+ * Testing {@link ${package}.ExampleMacro}
+ */
+public class TestExampleServlet extends TestCase
+{
+    public void testBasic()
+    {
+        // add test here...
+    }
+}
