95451014c0ad1a186b4062d01974aeda1ec85690
bspeakmon <bspeakmon@atlassian.com>
2010-03-01 00:24:49 +0000

    - AMPS-247: try again to do confluence 3.1.1
    git-svn-id: https://studio.atlassian.com/svn/AMPS/branches/3.0-stable@56014 2c54a935-e501-0410-bc05-97a93f6bca70

--- amps-product-archetypes/confluence-plugin-archetype/src/main/resources/archetype-resources/src/test/java/it/AbstractIntegrationTestCase.java
+++ amps-product-archetypes/confluence-plugin-archetype/src/main/resources/archetype-resources/src/test/java/it/AbstractIntegrationTestCase.java
@@ -1,21 +1,44 @@
 package it;
 
 import com.atlassian.confluence.plugin.functest.AbstractConfluencePluginWebTestCase;
-import com.atlassian.confluence.plugin.functest.ConfluenceWebTester;
+import com.atlassian.confluence.plugin.functest.JWebUnitConfluenceWebTester;
+import com.atlassian.confluence.plugin.functest.TesterConfiguration;
+import junit.framework.Assert;
+
+import java.io.IOException;
+import java.util.Properties;
 
 public class AbstractIntegrationTestCase extends AbstractConfluencePluginWebTestCase
 {
     @Override
-    protected ConfluenceWebTester createConfluenceWebTester()
+    protected JWebUnitConfluenceWebTester createConfluenceWebTester()
     {
-        ConfluenceWebTester tester = new ConfluenceWebTester();
-        tester.setAdminUserName("admin");
-        tester.setAdminPassword("admin");
-        tester.setHostName("localhost");
-        tester.setPort(Integer.parseInt(System.getProperty("http.port")));
-        tester.setContextPath(System.getProperty("context.path"));
-        tester.setProtocol("http");
-        tester.setSoapServicePath("/rpc/soap-axis/confluenceservice-v1?wsdl");
+        Properties props = new Properties();
+        props.put("confluence.webapp.protocol", "http");
+        props.put("confluence.webapp.host", "localhost");
+
+        // this is deceiving: the func test library checks for the system properties
+        // *before* checking in this properties file for these values, so these
+        // properties are technically ignored
+        props.put("confluence.webapp.port", Integer.parseInt(System.getProperty("http.port")));
+        props.put("confluence.webapp.context.path", System.getProperty("context.path"));
+
+        props.put("confluence.auth.admin.username", "admin");
+        props.put("confluence.auth.admin.password", "admin");
+
+        TesterConfiguration conf;
+        try
+        {
+            conf = new TesterConfiguration(props);
+        }
+        catch (IOException ioe)
+        {
+            Assert.fail("Unable to create tester: " + ioe.getMessage());
+            return null;
+        }
+
+        JWebUnitConfluenceWebTester tester = new JWebUnitConfluenceWebTester(conf);
+
         tester.getTestContext().setBaseUrl(tester.getBaseUrl());
         tester.setScriptingEnabled(false);
 
