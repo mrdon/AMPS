569361bbf341c05d162a2fcb502a85f416ad43c1
dbrown <dbrown@atlassian.com>
2010-06-09 08:01:10 +0000

    Fixing shitty tests with new test group name in test results...may change later
    git-svn-id: https://studio.atlassian.com/svn/AMPS/trunk@61639 2c54a935-e501-0410-bc05-97a93f6bca70

--- maven-amps-plugin/src/it/integrationTest/validate.groovy
+++ maven-amps-plugin/src/it/integrationTest/validate.groovy
@@ -6,7 +6,7 @@ product = 'amps'.equals(product) ? 'refapp' : product
 def surefireReports = new File(basedir, 'target/shitty-it-surefire-reports')
 assert !surefireReports.exists(), "No unit test should have been run, see $surefireReports.absolutePath"
 
-def integrationSurefireReports = new File(basedir, "target/$product/tomcat6x/surefire-reports")
+def integrationSurefireReports = new File(basedir, "target/group-__no_test_group__/tomcat6x/surefire-reports")
 assert integrationSurefireReports.exists(), "Integration tests should have run and created test reports in $integrationSurefireReports"
 
 assert new File(integrationSurefireReports, 'it.com.atlassian.amps.IntegrationTest.txt').exists(), "IntegrationTest.txt file did not exist"
