085693c575828fe5fcd832dce1090f68f4e3c64f
doklovic <doklovic@atlassian.com>
2011-07-20 21:43:25 +0000

    AMPS-450: Generator: Common - Web Panel
    git-svn-id: https://studio.atlassian.com/svn/AMPS/branches/plugin-module-generation@127267 2c54a935-e501-0410-bc05-97a93f6bca70

---
+++ plugin-module-codegen-engine/src/main/java/com/atlassian/plugins/codegen/modules/common/web/WebPanelModuleCreator.java
@@ -0,0 +1,36 @@
+package com.atlassian.plugins.codegen.modules.common.web;
+
+import com.atlassian.plugins.codegen.annotations.*;
+import com.atlassian.plugins.codegen.modules.AbstractPluginModuleCreator;
+import com.atlassian.plugins.codegen.modules.PluginModuleLocation;
+import com.atlassian.plugins.codegen.modules.PluginModuleProperties;
+
+/**
+ * @since version
+ */
+@RefAppPluginModuleCreator
+@JiraPluginModuleCreator
+@ConfluencePluginModuleCreator
+@BambooPluginModuleCreator
+@Dependencies({
+        @Dependency(groupId = "org.mockito", artifactId = "mockito-all", version = "1.8.5", scope = "test")
+})
+public class WebPanelModuleCreator extends AbstractPluginModuleCreator<WebPanelProperties> {
+
+    public static final String MODULE_NAME = "Web Panel";
+    private static final String TEMPLATE_PREFIX = "templates/common/web/webpanel/";
+
+    private static final String PLUGIN_MODULE_TEMPLATE = TEMPLATE_PREFIX + "web-panel-plugin.xml.vtl";
+
+    @Override
+    public void createModule(PluginModuleLocation location, WebPanelProperties props) throws Exception {
+
+        addModuleToPluginXml(PLUGIN_MODULE_TEMPLATE, location, props);
+    }
+
+
+    @Override
+    public String getModuleName() {
+        return MODULE_NAME;
+    }
+}
---
+++ plugin-module-codegen-engine/src/main/java/com/atlassian/plugins/codegen/modules/common/web/WebPanelProperties.java
@@ -0,0 +1,31 @@
+package com.atlassian.plugins.codegen.modules.common.web;
+
+import com.atlassian.plugins.codegen.modules.BasicClassModuleProperties;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @since version
+ */
+public class WebPanelProperties extends AbstractWebFragmentProperties {
+
+    public static final String LOCATION = "LOCATION";
+
+    public WebPanelProperties() {
+        this("My Web Panel");
+    }
+
+    public WebPanelProperties(String moduleName) {
+        super(moduleName);
+    }
+
+    public WebPanelProperties(String moduleName, String location) {
+        this(moduleName);
+        setLocation(location);
+    }
+
+    public void setLocation(String location) {
+        setProperty(LOCATION,location);
+    }
+}
