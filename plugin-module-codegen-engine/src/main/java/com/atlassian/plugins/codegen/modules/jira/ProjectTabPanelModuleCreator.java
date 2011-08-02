241dc5a63068580b13d9b3e88be518fb324f079f
doklovic <doklovic@atlassian.com>
2011-08-02 21:57:21 +0000

    AMPS-464: Generator: JIRA - Project Tab Panel
    git-svn-id: https://studio.atlassian.com/svn/AMPS/branches/plugin-module-generation@127810 2c54a935-e501-0410-bc05-97a93f6bca70

---
+++ plugin-module-codegen-engine/src/main/java/com/atlassian/plugins/codegen/modules/jira/ProjectTabPanelModuleCreator.java
@@ -0,0 +1,69 @@
+package com.atlassian.plugins.codegen.modules.jira;
+
+import com.atlassian.plugins.codegen.annotations.Dependencies;
+import com.atlassian.plugins.codegen.annotations.Dependency;
+import com.atlassian.plugins.codegen.annotations.JiraPluginModuleCreator;
+import com.atlassian.plugins.codegen.modules.AbstractPluginModuleCreator;
+import com.atlassian.plugins.codegen.modules.PluginModuleLocation;
+
+import java.io.File;
+
+/**
+ * @since version
+ */
+@JiraPluginModuleCreator
+@Dependencies({
+        @Dependency(groupId = "org.mockito", artifactId = "mockito-all", version = "1.8.5", scope = "test")
+})
+public class ProjectTabPanelModuleCreator extends AbstractPluginModuleCreator<TabPanelProperties> {
+
+    public static final String GENERIC_CLASS = "GenericProjectTabPanel";
+    public static final String GENERIC_PACKAGE = "com.atlassian.jira.plugin.projectpanel.impl";
+    public static final String FQ_GENERIC_CLASS = GENERIC_PACKAGE + "." + GENERIC_CLASS;
+
+    public static final String MODULE_NAME = "Project Tab Panel";
+    private static final String TEMPLATE_PREFIX = "templates/jira/tabpanel/project/";
+
+    //stub
+    private static final String CLASS_TEMPLATE = TEMPLATE_PREFIX + "ProjectTabPanel.java.vtl";
+    private static final String UNIT_TEST_TEMPLATE = "templates/generic/GenericTest.java.vtl";
+    private static final String FUNC_TEST_TEMPLATE = TEMPLATE_PREFIX + "ProjectTabPanelFuncTest.java.vtl";
+    private static final String VIEW_TEMPLATE = TEMPLATE_PREFIX + "project-tab-panel.vm.vtl";
+
+    //examples
+    private static final String EXAMPLE_CLASS_TEMPLATE = TEMPLATE_PREFIX + "Example" + CLASS_TEMPLATE;
+
+    private static final String PLUGIN_MODULE_TEMPLATE = TEMPLATE_PREFIX + "project-tab-panel-plugin.xml.vtl";
+
+    @Override
+    public void createModule(PluginModuleLocation location, TabPanelProperties props) throws Exception {
+        String moduleKey = props.getModuleKey();
+        String packageName = props.getPackage();
+        String classname = props.getClassname();
+        String viewFileName = moduleKey + ".vm";
+        File templatesDir = new File(location.getTemplateDirectory(), "tabpanels");
+
+        if (props.includeExamples()) {
+            templateHelper.writeJavaClassFromTemplate(EXAMPLE_CLASS_TEMPLATE, classname, location.getSourceDirectory(), packageName, props);
+        } else {
+            if (props.isUseCustomClass()) {
+                //main class
+                templateHelper.writeJavaClassFromTemplate(CLASS_TEMPLATE, classname, location.getSourceDirectory(), packageName, props);
+
+                //unit test
+                templateHelper.writeJavaClassFromTemplate(UNIT_TEST_TEMPLATE, testClassname(classname), location.getTestDirectory(), packageName, props);
+            }
+
+            templateHelper.writeFileFromTemplate(VIEW_TEMPLATE, viewFileName, templatesDir, props);
+        }
+
+
+        addModuleToPluginXml(PLUGIN_MODULE_TEMPLATE, location, props);
+    }
+
+
+    @Override
+    public String getModuleName() {
+        return MODULE_NAME;
+    }
+}
