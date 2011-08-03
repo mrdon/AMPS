34cfcafb8569bbf153c2878b12c8f44e1cb7497a
doklovic <doklovic@atlassian.com>
2011-08-03 18:19:54 +0000

    AMPS-469: Generator: JIRA - Version Tab Panel
    git-svn-id: https://studio.atlassian.com/svn/AMPS/branches/plugin-module-generation@127850 2c54a935-e501-0410-bc05-97a93f6bca70

--- plugin-module-codegen-engine/src/main/java/com/atlassian/plugins/codegen/modules/jira/VersionTabPanelModuleCreator.java
+++ plugin-module-codegen-engine/src/main/java/com/atlassian/plugins/codegen/modules/jira/VersionTabPanelModuleCreator.java
@@ -14,25 +14,25 @@ import java.io.File;
 @Dependencies({
         @Dependency(groupId = "org.mockito", artifactId = "mockito-all", version = "1.8.5", scope = "test")
 })
-public class ComponentTabPanelModuleCreator extends AbstractPluginModuleCreator<TabPanelProperties> {
+public class VersionTabPanelModuleCreator extends AbstractPluginModuleCreator<TabPanelProperties> {
 
     public static final String GENERIC_CLASS = "GenericTabPanel";
-    public static final String GENERIC_PACKAGE = "com.atlassian.jira.plugin.componentpanel.impl";
+    public static final String GENERIC_PACKAGE = "com.atlassian.jira.plugin.versionpanel.impl";
     public static final String FQ_GENERIC_CLASS = GENERIC_PACKAGE + "." + GENERIC_CLASS;
 
-    public static final String MODULE_NAME = "Component Tab Panel";
-    private static final String TEMPLATE_PREFIX = "templates/jira/tabpanel/component/";
+    public static final String MODULE_NAME = "Version Tab Panel";
+    private static final String TEMPLATE_PREFIX = "templates/jira/tabpanel/version/";
 
     //stub
-    private static final String CLASS_TEMPLATE = TEMPLATE_PREFIX + "ComponentTabPanel.java.vtl";
+    private static final String CLASS_TEMPLATE = TEMPLATE_PREFIX + "VersionTabPanel.java.vtl";
     private static final String UNIT_TEST_TEMPLATE = "templates/generic/GenericTest.java.vtl";
-    private static final String FUNC_TEST_TEMPLATE = TEMPLATE_PREFIX + "ComponentTabPanelFuncTest.java.vtl";
-    private static final String VIEW_TEMPLATE = TEMPLATE_PREFIX + "component-tab-panel.vm.vtl";
+    private static final String FUNC_TEST_TEMPLATE = TEMPLATE_PREFIX + "VersionTabPanelFuncTest.java.vtl";
+    private static final String VIEW_TEMPLATE = TEMPLATE_PREFIX + "version-tab-panel.vm.vtl";
 
     //examples
     private static final String EXAMPLE_CLASS_TEMPLATE = TEMPLATE_PREFIX + "Example" + CLASS_TEMPLATE;
 
-    private static final String PLUGIN_MODULE_TEMPLATE = TEMPLATE_PREFIX + "component-tab-panel-plugin.xml.vtl";
+    private static final String PLUGIN_MODULE_TEMPLATE = TEMPLATE_PREFIX + "version-tab-panel-plugin.xml.vtl";
 
     @Override
     public void createModule(PluginModuleLocation location, TabPanelProperties props) throws Exception {
