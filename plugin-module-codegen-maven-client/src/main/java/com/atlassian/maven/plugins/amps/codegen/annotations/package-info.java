df7a955354947235aeceb482d11e1520482f55f2
doklovic <doklovic@atlassian.com>
2011-09-26 17:44:32 +0000

    AMPS-575 - Add package-info.java
    git-svn-id: https://studio.atlassian.com/svn/AMPS/branches/plugin-module-generation@130186 2c54a935-e501-0410-bc05-97a93f6bca70

---
+++ plugin-module-codegen-maven-client/src/main/java/com/atlassian/maven/plugins/amps/codegen/annotations/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Provides annotations used in the maven client.
+ *
+ * Particularly, the ModuleCreatorClass annotation that's used to link Prompters with their respective ModuleCreators
+ *
+ * @since 3.6
+ *
+ * @see ModuleCreatorClass
+ */
+package com.atlassian.maven.plugins.amps.codegen.annotations;
---
+++ plugin-module-codegen-maven-client/src/main/java/com/atlassian/maven/plugins/amps/codegen/prompter/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Provides Maven Prompters to collect information from the user and create the proper Properties class to be passed
+ * to the ModuleCreator.
+ *
+ * These essentially make up the client UI for plugin module generation in maven.
+ *
+ * @since 3.6
+ *
+ * @see AbstractModulePrompter
+ */
+package com.atlassian.maven.plugins.amps.codegen.prompter;
