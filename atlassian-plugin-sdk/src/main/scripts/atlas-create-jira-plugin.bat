965bd875d51cdb358f695dfab9693b60d3288b21
doklovic <doklovic@atlassian.com>
2011-10-07 21:19:15 +0000

    AMPS-594: Improve the JIRA4 vs. JIRA 5 mess
    git-svn-id: https://studio.atlassian.com/svn/AMPS/branches/3.6-stable@130609 2c54a935-e501-0410-bc05-97a93f6bca70

---
+++ atlassian-plugin-sdk/src/main/scripts/atlas-create-jira-plugin.bat
@@ -0,0 +1,28 @@
+
+
+@echo off
+if "%OS%" == "Windows_NT" setlocal enabledelayedexpansion
+
+rem ---------------------------------------------------------------
+rem Find absolute path to the program
+rem ---------------------------------------------------------------
+
+set PRGDIR=%~dp0
+set CURRENTDIR=%cd%
+cd /d %PRGDIR%..
+set ATLAS_HOME=%cd%
+cd /d %CURRENTDIR%
+
+echo A new plugin structure is required for JIRA 5 plugins due to significant changes in the APIs.
+echo Create a plugin for?
+:prompt
+echo 1) Shiny new JIRA 5
+echo 2) Regular 'ol JIRA 4 (or earlier)
+SET /P useJira5="#?"
+if not "%useJira5%" == "1" if not "%useJira5%" == "2" goto prompt
+
+if "%useJira5%" == "1" (
+	atlas-create-jira5-plugin.bat
+) else (
+	atlas-create-jira4-plugin.bat
+)
