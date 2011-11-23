ca0d556cf1dca3528ca63675f244057f21573a4e
aragot <aragot@atlassian.com>
2011-11-23 01:17:44 +0000

    Reverting an useful version bumpup
    git-svn-id: https://studio.atlassian.com/svn/AMPS/branches/caviar-support@132098 2c54a935-e501-0410-bc05-97a93f6bca70

--- maven-amps-plugin/src/main/java/com/atlassian/maven/plugins/amps/MavenGoals.java
+++ maven-amps-plugin/src/main/java/com/atlassian/maven/plugins/amps/MavenGoals.java
@@ -67,7 +67,7 @@ public class MavenGoals
             // Below is a second definition of 'cargo-maven2-plugin', using CodeHaus instead of TwData.
             put("org.codehaus.cargo:cargo-maven2-plugin", "1.1.3");
             put("atlassian-pdk", "2.3.0");
-            put("maven-archetype-plugin", "2.1");
+            put("maven-archetype-plugin", "2.0-alpha-4");
             put("maven-bundle-plugin", "2.0.0");
             put("yuicompressor-maven-plugin", "0.7.1");
             put("build-helper-maven-plugin", "1.7");
